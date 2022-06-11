39b0aba741cc80e8af36ba97118c314332baf6ed
Zheng Xu <zhengxu7806@gmail.com>
2022-06-11 15:26:15 -0400

    add file ext name and use output folder

--- src/images/resize.ts
+++ src/images/resize.ts
@@ -4,10 +4,13 @@ import { WYSE_JSON } from '../packager/WyseManifest'
 import * as fs from 'fs'
 import imageSize from 'image-size'
 import { convert } from 'imagemagick'
+import { charsets } from 'mime-types'
+import lookup = charsets.lookup
 const chalk = require('chalk')
 
 const resizeImages = (folder: string, configPath?: string) => {
   let configFilePath = ''
+  const outputFolderName = `${folder}_output`
   if (!configPath) {
     configFilePath = path.join(folder, WYSE_JSON)
     if (!fs.existsSync(configFilePath)) {
@@ -17,6 +20,9 @@ const resizeImages = (folder: string, configPath?: string) => {
     configFilePath = configPath
   }
   try {
+    if (fs.existsSync(outputFolderName)) {
+      fs.rmdirSync(outputFolderName)
+    }
     const data = fs.readFileSync(configFilePath, {encoding: 'utf-8'})
     const configJson = JSON.parse(data) as WyseConfig
     if (!configJson.width || configJson.width <= 10) {
@@ -26,20 +32,22 @@ const resizeImages = (folder: string, configPath?: string) => {
     const imageWidth = configJson.width as number
     let files = fs.readdirSync(folder)
     files = files.filter((name)=> {
-      return name !== WYSE_JSON
+      const mimetype = lookup(name)
+      return mimetype && mimetype.includes('image')
     })
     files.sort((a, b) => {
       return a.length - b.length
     })
     files.forEach((fileName, index) => {
       const filePath = path.join(folder, fileName)
-      const outputPath = path.join(folder, `image_${index.toString().padStart(4, '0')}`)
+      const ext = path.extname(filePath)
+      const outputPath = path.join(outputFolderName, `image_${index.toString().padStart(4, '0')}`) + ext
       const imageDimensions = imageSize(filePath)
       if (imageDimensions.width && imageDimensions.height) {
         const ratio = imageWidth / imageDimensions.width
         let imageHeight = imageDimensions.height * ratio
         imageHeight = Math.round(imageHeight)
-        console.log(`converting image ${fileName} from size ${imageDimensions.width} x ${imageDimensions.height} to ${imageWidth} x ${imageHeight}`)
+        console.log(`converting image ${fileName} from size ${imageDimensions.width} x ${imageDimensions.height} to ${imageWidth} x ${imageHeight} and saving to ${outputPath}`)
         convert([filePath, '-resize', `${imageWidth}x${imageHeight}`, outputPath], (error, output) => {
           if (error) {
             throw error
